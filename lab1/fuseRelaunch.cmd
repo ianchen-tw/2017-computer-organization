926b6679a45f6ae1222ffc4f56078ce7996af5e9
Ian Chen 陳奕安 <ianre657@gmail.com>
2017-08-04 23:02:54 +0800

    untrack auto generated files
    只留下程式碼跟說明文件

--- lab1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "Y:/Desktop/ComputerOrgan/code/lab1/testbench_isim_beh.exe" -prj "Y:/Desktop/ComputerOrgan/code/lab1/testbench_beh.prj" "work.testbench" "work.glbl" 
